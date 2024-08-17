d41fa8ec4cb980df8bc35e3f15bc5c0ce990e816
kanchankhemnar <kanchankhemnar170@gmail.com>
2024-08-17 17:32:10 +0530

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SIgnUp";
+import SignUp from "./pages/SignUp/SignUp";
 
 const App = () => {
   return (
